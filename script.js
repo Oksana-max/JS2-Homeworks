6d37fd8d11e82fa2526292a0af41273b793670aa
OksanaPavlova <oksana-91-oks@mail.ru>
2021-04-22 22:20:54 +0300

    Homework2 chanches

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 class GoodsItem {
-    constructor(title, price, quantity) {
+    constructor(title, price, ) {
         this.title = title;
         this.price = price;
-        this.quantity = 0;
-       
     }
     render() {
-        return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p><p>${this.quantity}</p></div>`;
+        return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
     }
 }
-
-
 
 class GoodsList {
     constructor() {
@@ -18,36 +14,38 @@ class GoodsList {
     }
     fetchGoods() {
         this.goods = [
-            { title: 'Shirt', price: 150, quantity: 0},
-            { title: 'Socks', price: 50, quantity: 0},
-            { title: 'Jacket', price: 350, quantity: 0},
-            { title: 'Shoes', price: 250, quantity: 0},
+            { title: 'Shirt', price: 150, },
+            { title: 'Socks', price: 50, },
+            { title: 'Jacket', price: 350, },
+            { title: 'Shoes', price: 250, },
         ];
 
     }
     render() {
         let listHtml = '';
         this.goods.forEach(good => {
-            const goodItem = new GoodsItem(good.title, good.price, good.quantity);
+            const goodItem = new GoodsItem(good.title, good.price, );
             listHtml += goodItem.render();
         });
         document.querySelector('.goods-list').innerHTML = listHtml;
     }
-    getSum(){
-            return this.price * this.quantity;
+
+    getTotalSum() {
+        return this.goods.forEach((item) => totalSum += item.price);
     }
+
 }
 
-class Basket{
-    constructor(){
-        this.goods = [];
+class GoodsCard extends GoodsList {
+    constructor() {
+        super();
     }
 }
-
-class BasketItem{
-    constructor(){}
+class CartItem extends GoodsItem {
+    constructor() {
+        super();
+    }
 }
-        
 
 
 //   Теперь, чтобы вывести список, нужно создать экземпляр класса GoodsList, вызвать для него метод fetchGoods, чтобы записать список товаров в свойство goods, и вызвать render().
@@ -55,15 +53,3 @@ const list = new GoodsList();
 list.fetchGoods();
 list.render();
 list.getSum();
-
-
-
-
-
-
-
-
-
-
-
-
