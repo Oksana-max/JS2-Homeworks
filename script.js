73d840257c0b1fa6e7770a0bc2d4a8ace8fe1365
OksanaPavlova <oksana-91-oks@mail.ru>
2021-04-24 21:41:36 +0300

    Homework3

--- script.js
+++ script.js
@@ -5,28 +5,31 @@
 //  /deleteFromBasket.json – удалить товар из корзины.
 
 
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
+function makeGETRequest(url) {
+    return new Promise((resolve, reject) => {
+        let xhr;
 
-function makeGETRequest(url, callback) {
-    var xhr;
-
-    if (window.XMLHttpRequest) {
-        xhr = new XMLHttpRequest();
-    } else if (window.ActiveXObject) {
-        xhr = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-            callback(xhr.responseText);
+        if (window.XMLHttpRequest) {
+            xhr = new XMLHttpRequest();
+        } else if (window.ActiveXObject) {
+            xhr = new ActiveXObject("Microsoft.XMLHTTP");
         }
-    }
 
-    xhr.open('GET', url, true);
-    xhr.send();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) resolve(xhr.responseText)
+                else reject('Error')
+            }
+        }
+
+        xhr.open('GET', `${API_URL}${url}`, true);
+        xhr.send();
+    })
 };
 
-const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
 
 
 
@@ -45,11 +48,12 @@ class GoodsList {
     constructor() {
         this.goods = [];
     }
-    fetchGoods(callback) {
-        makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
-            this.goods = JSON.parse(goods);
-            callback();
-        })
+    fetchGoods() {
+        return makeGETRequest(`/catalogData.json`)
+            .then((goods) => {
+                this.goods = JSON.parse(goods);
+                this.filteredGoods = JSON.parse(goods);
+            })
     }
 
     render() {
@@ -71,16 +75,27 @@ class GoodsCard extends GoodsList {
     constructor() {
         super();
     }
+
+    fetchGoods() {}
+
+    clear() {}
+
+    buyAll() {}
 }
 class CartItem extends GoodsItem {
     constructor() {
         super();
     }
+
+    delete() {}
+
+    addOne() {}
+
+    removeOne() {}
 }
 
 
 //   Теперь, чтобы вывести список, нужно создать экземпляр класса GoodsList, вызвать для него метод fetchGoods, чтобы записать список товаров в свойство goods, и вызвать render().
 const list = new GoodsList();
-list.fetchGoods(() => {
-    list.render();
-});
+list.fetchGoods()
+    .then(() => list.render());
