a571166e929cc20397527d444e9a6231ac938df1
OksanaPavlova <oksana-91-oks@mail.ru>
2021-04-29 09:19:40 +0300

    Lesson 5

--- script.js
+++ script.js
@@ -1,86 +1,46 @@
-//  https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses – адрес API;
-//  /catalogData.json – получить список товаров;
-//  /getBasket.json – получить содержимое корзины;
-//  /addToBasket.json – добавить товар в корзину;
-//  /deleteFromBasket.json – удалить товар из корзины.
-
-
-
-
-function makeGETRequest(url, callback) {
-    var xhr;
-
-    if (window.XMLHttpRequest) {
-        xhr = new XMLHttpRequest();
-    } else if (window.ActiveXObject) {
-        xhr = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-            callback(xhr.responseText);
-        }
-    }
-
-    xhr.open('GET', url, true);
-    xhr.send();
-};
-
 const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
+const app = new Vue({
+    el: '#app',
+    data: () => ({
+        goods: [],
+        filteredGoods: [],
+        searchLine: ''
+    }),
+    mounted() {
+        this.makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
+            // API который мы исполььзуем возвращает нам массив в строчном представлении (JSON)
+            // для работы с ним в приложение, необходимо выполнить процесс десериализации
 
-
-class GoodsItem {
-    constructor(product_name, price, ) {
-        this.product_name = product_name;
-        this.price = price;
-    }
-    render() {
-        return `<div class="goods-item"><h3>${this.product_name}</h3><p>${this.price}</p></div>`;
-    }
-}
-
-
-class GoodsList {
-    constructor() {
-        this.goods = [];
-    }
-    fetchGoods(callback) {
-        makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
+            // к вопросу о том, почему в доме появлялось там много пустых значений -> строка это в некотором роде массив букв
+            // vue просто проходился по нашей строке и пытался работать с каждой буквой в строке как с отдельным объектом
             this.goods = JSON.parse(goods);
-            callback();
-        })
-    }
-
-    render() {
-        let listHtml = '';
-        this.goods.forEach(good => {
-            const goodItem = new GoodsItem(good.product_name, good.price, );
-            listHtml += goodItem.render();
+            this.filteredGoods = JSON.parse(goods);
         });
-        document.querySelector('.goods-list').innerHTML = listHtml;
+    },
+    methods: {
+        makeGETRequest(url, callback) {
+            var xhr;
+
+            if (window.XMLHttpRequest) {
+                xhr = new XMLHttpRequest();
+            } else if (window.ActiveXObject) {
+                xhr = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === 4) {
+                    callback(xhr.responseText);
+                }
+            }
+
+            xhr.open('GET', url, true);
+            xhr.send();
+        }
+    },
+    watch: {
+        filteredGoods() {
+            console.log('this.filteredGoods :>> ', this.filteredGoods);
+        }
     }
-
-    getTotalSum() {
-        return this.goods.forEach((item) => totalSum += item.price);
-    }
-
-}
-
-class GoodsCard extends GoodsList {
-    constructor() {
-        super();
-    }
-}
-class CartItem extends GoodsItem {
-    constructor() {
-        super();
-    }
-}
-
-
-//   Теперь, чтобы вывести список, нужно создать экземпляр класса GoodsList, вызвать для него метод fetchGoods, чтобы записать список товаров в свойство goods, и вызвать render().
-const list = new GoodsList();
-list.fetchGoods(() => {
-    list.render();
 });
